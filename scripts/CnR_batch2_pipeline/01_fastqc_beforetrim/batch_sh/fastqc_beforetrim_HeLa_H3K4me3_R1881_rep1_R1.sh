6406475451c465da48fbc8c28cbc31f2bc6c9e5d
Christophe Tav <christophe.tav@gmail.com>
2024-03-18 15:24:00 -0400

    update

---
+++ scripts/CnR_batch2_pipeline/01_fastqc_beforetrim/batch_sh/fastqc_beforetrim_HeLa_H3K4me3_R1881_rep1_R1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --account=def-stbil30
+#SBATCH --mail-user=christophe.tav@gmail.com
+#SBATCH --mail-type=ALL
+
+
+mkdir -p /home/chris11/projects/def-stbil30/chris11/20240318_CnR_batch2/output/cnr-pipeline_batch2-GRCh38_PE/fastqc_beforetrim_output/HeLa_H3K4me3_R1881_rep1_R1
+
+
+/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/fastqc/0.11.9/fastqc --outdir /home/chris11/projects/def-stbil30/chris11/20240318_CnR_batch2/output/cnr-pipeline_batch2-GRCh38_PE/fastqc_beforetrim_output/HeLa_H3K4me3_R1881_rep1_R1 --format fastq /home/chris11/projects/def-stbil30/chris11/20240318_CnR_batch2/raw/batch2/raw_fastq/NS.LH00487_0035.001.UDI0015_i7---UDI0015_i5.SB-FK-UL_HELA_CnR_H3K4met3_R1881_10nM_30min_R1_R1.fastq.gz
---
+++ scripts/CnR_batch2_pipeline/01_fastqc_beforetrim/batch_sh/fastqc_beforetrim_VCaP_IgG_EtOH_rep1_R2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --account=def-stbil30
+#SBATCH --mail-user=christophe.tav@gmail.com
+#SBATCH --mail-type=ALL
+
+
+mkdir -p /home/chris11/projects/def-stbil30/chris11/20240318_CnR_batch2/output/cnr-pipeline_batch2-GRCh38_PE/fastqc_beforetrim_output/VCaP_IgG_EtOH_rep1_R2
+
+
+/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/fastqc/0.11.9/fastqc --outdir /home/chris11/projects/def-stbil30/chris11/20240318_CnR_batch2/output/cnr-pipeline_batch2-GRCh38_PE/fastqc_beforetrim_output/VCaP_IgG_EtOH_rep1_R2 --format fastq /home/chris11/projects/def-stbil30/chris11/20240318_CnR_batch2/raw/batch2/raw_fastq/NS.LH00487_0035.001.UDI0018_i7---UDI0018_i5.SB-FK-UL_VCAP_CnR_IgG_EtOH_30min_R1_R2.fastq.gz
